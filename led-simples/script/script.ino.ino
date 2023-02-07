72189b91a79ac2f79bc945aa4cbdea164cffeeef
gvheisler <gvheisler@inf.ufsm.br>
2023-02-07 12:44:40 -0300

    nome modificado

--- led-simples/script/script.ino.ino
+++ led-simples/script/script.ino.ino
@@ -8,7 +8,7 @@ void setup()
 void loop()
 {
   digitalWrite(2, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000);
   digitalWrite(2, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000);
 }
